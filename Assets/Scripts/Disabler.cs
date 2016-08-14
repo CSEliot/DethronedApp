d9358b8fa0dde31697ab1ba915ff4edf41c29ead
Eliot <ecsit39@gmail.com>
2016-08-13 20:30:46 -0400

    Layout begun, not finished.

---
+++ Assets/Scripts/Disabler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Disabler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void DisableNow()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void DisableParent()
+    {
+        transform.parent.gameObject.SetActive(false);
+    }
+}
