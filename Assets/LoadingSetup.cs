bee67a95722d7cb148b6625bb6429b8d521e9a9b
Eliot <Eliot@localhost>
2016-08-13 20:30:46 -0400

    Layout begun, not finished.

---
+++ Assets/LoadingSetup.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadingSetup : MonoBehaviour {
+
+    public GameObject[] DisableOnStart;
+    public GameObject[] EnableOnStart;
+
+	// Use this for initialization
+	void Start () {
+	    for(int i = 0; i < DisableOnStart.Length; i++)
+        {
+            DisableOnStart[i].SetActive(false);
+        }
+        for (int i = 0; i < EnableOnStart.Length; i++)
+        {
+            DisableOnStart[i].SetActive(true);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
