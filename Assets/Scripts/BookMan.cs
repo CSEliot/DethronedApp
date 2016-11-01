b5e376a90e7c49d2bc8f9ce5864e6894a42caa76
Eliot Carney-Seim <eliot@kitelion.me>
2016-11-01 18:40:52 -0400

    FINISHED!

---
+++ Assets/Scripts/BookMan.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BookMan : MonoBehaviour {
+
+    public GameObject[] Pages;
+
+    public static GameObject[] _Pages;
+
+    private static GameObject currentPage;
+    private static int currentNum;
+    private static int MAX_CARDS;
+
+    private bool fixedFirst = false;
+
+    // Use this for initialization
+    void Start () {
+        transform.tag = "BookMan";
+        MAX_CARDS = Pages.Length;
+        _Pages = Pages;
+        currentNum = 0;
+        currentPage = _Pages[currentNum];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!fixedFirst) {
+            _Pages[0].GetComponent<Animator>().SetBool("IsIn", true);
+            fixedFirst = true;
+        }
+
+    }
+
+    /// <summary>
+    /// Goes from MAX_CARDS -> 0
+    /// </summary>
+    public static void RoleUp (){
+        CBUG.Do("Activated Up!");
+        if (currentNum - 1 <= 0) //==1 because we never move up last card
+            return;
+        currentNum--;
+        currentPage = _Pages[currentNum];
+        currentPage.GetComponent<Animator>().SetBool("IsIn", false);
+    }
+
+    /// <summary>
+    /// Goes from 0 -> MAX_CARDS
+    /// </summary>
+    public static void RoleDown()
+    {
+        CBUG.Do("Activated Down!");
+        if (currentNum + 1 > _Pages.Length)
+            return;
+        currentPage.GetComponent<Animator>().SetBool("IsIn", true);
+        currentNum++;
+        currentPage = _Pages[currentNum >= _Pages.Length ? _Pages.Length - 1 : currentNum];
+    }
+
+    public static CardMan GetRef()
+    {
+        return GameObject.FindGameObjectWithTag("BookMan").GetComponent<CardMan>();
+    }
+}
---
+++ Assets/Scripts/CardMan.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardMan : MonoBehaviour {
+
+    public GameObject[] Cards;
+
+    public static GameObject[] _Cards;
+
+    private static GameObject currentCard;
+    private static int currentNum;
+    private static int MAX_CARDS;
+
+    private bool fixedFirst = false;
+
+	// Use this for initialization
+	void Start () {
+        transform.tag = "CardMan";
+        MAX_CARDS = Cards.Length;
+        _Cards = Cards;
+        currentNum = 0;
+        currentCard = _Cards[currentNum];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!fixedFirst) {
+            _Cards[0].GetComponent<Animator>().SetBool("IsIn", true);
+            fixedFirst = true;
+        }
+
+        CBUG.Do("CurrentNum: " + currentNum);
+
+	}
+
+    /// <summary>
+    /// Goes from MAX_CARDS -> 0
+    /// </summary>
+    public static void RoleUp (){
+        CBUG.Do("Activated Up!");
+        if (currentNum - 1 <= 0) //==1 because we never move up last card
+            return;
+        currentNum--;
+        currentCard = _Cards[currentNum];
+        currentCard.GetComponent<Animator>().SetBool("IsIn", false);
+    }
+
+    /// <summary>
+    /// Goes from 0 -> MAX_CARDS
+    /// </summary>
+    public static void RoleDown()
+    {
+        CBUG.Do("Activated Down!");
+        if (currentNum + 1 > _Cards.Length)
+            return;
+        currentCard.GetComponent<Animator>().SetBool("IsIn", true);
+        currentNum++;
+        currentCard = _Cards[currentNum >= _Cards.Length ? _Cards.Length - 1 : currentNum];
+    }
+
+    public static CardMan GetRef()
+    {
+        return GameObject.FindGameObjectWithTag("CardMan").GetComponent<CardMan>();
+    }
+}
